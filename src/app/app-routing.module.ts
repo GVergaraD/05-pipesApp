d2c61efdc1064eb5e18e4a22406e25f4e4bc1a7c
Gabriel Vergara <gvergara182@gmail.com>
2024-12-28 01:05:27 -0300

    Fin sección 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BasicsPageComponent } from './products/pages/basics-page/basics-page.component';
-import { NumbersPageComponent } from './products/pages/numbers-page/numbers-page.component';
-import { UncommonPageComponent } from './products/pages/uncommon-page/uncommon-page.component';
 
 const routes: Routes = [
   {
-  path: '',
-  component: BasicsPageComponent
-},
-  {
-  path: 'numbers',
-  component: NumbersPageComponent
-},
-  {
-  path: 'uncommon',
-  component: UncommonPageComponent
-},
-  {
-  path: '**',
-  redirectTo: ''
-},
+    path: '',
+    loadChildren: () => import('./products/products.module').then( m => m.ProductsModule ),
+  }
 ];
 
 @NgModule({
